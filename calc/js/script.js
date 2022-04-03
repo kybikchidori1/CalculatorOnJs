2d5515c3ea5f512f0e644a18aa3e37e5ea174850
Roman <kybik_rybik1994@mail.ru>
2022-04-03 11:02:32 +0300

    first commit

---
+++ calc/js/script.js
@@ -0,0 +1,8 @@
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open(method, url, async);
+});
