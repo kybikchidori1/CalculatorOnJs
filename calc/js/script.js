4bf804f5f6b0d5bcf9508edfeca0ef358fa423c3
Roman <kybik_rybik1994@mail.ru>
2022-04-03 11:50:09 +0300

    pracice 52 - end

--- calc/js/script.js
+++ calc/js/script.js
@@ -4,5 +4,22 @@ const inputRub = document.querySelector('#rub'),
 inputRub.addEventListener('input', () => {
     const request = new XMLHttpRequest();
 
-    request.open(method, url, async);
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.response);
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = "Что-то пошло не так";
+        }
+    });
+
+    // status 
+    // statsText
+    // response
+    // readyState
 });
